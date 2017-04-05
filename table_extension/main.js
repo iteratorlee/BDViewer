e3081cc6b68f3274b926923da5663a6ae39f4bfb
iteratorlee <liyan19960701@pku.edu.cn>
2017-04-05 07:46:16 -0700

    bind events

--- table_extension/main.js
+++ table_extension/main.js
@@ -1,23 +1,26 @@
 define([
     'base/js/namespace',
     'base/js/utils',
+    'base/js/events',
     'services/config',
     'jqueryui'
 ], function(
     Jupyter,
     utils,
-    configmod,
+    events,
+    Configmod,
     $
 ) {
     'use strict';
     var base_url = utils.get_body_data('baseUrl');
 
-    var config = new Configmod.ConfigSection('tree', {base_url : base_url});
-    config.loaded.then(function(){
-      add_file_meta();
-    });
+    //var config = new Configmod.ConfigSection('tree', {base_url : base_url});
+    //config.loaded.then(function(){
+      //add_file_meta();
+    //});
 
     function add_file_meta(){
+        alert("hahahahsb");
         var row = $("div.list_item");
         var item = $("div.col-md-12");
         item.each(function(){
@@ -27,8 +30,10 @@ define([
             .width("80")
             .appendTo(this);
         });
+        //alert(item.length);
 
         var path = $(".item_link").attr("href");
+        //alert(path.length);
 
         var filesize = new Array();
         for(var i = 0; i < path.length; ++i){
@@ -42,28 +47,10 @@ define([
     }
 
     function load_ipython_extension() {
+        alert("sbsbsbsbsbsbsbsb");
+        events.on("notebook_loaded.Notebook", add_file_meta);
+        events.on("app_initialized.DashboardApp", add_file_meta);
         //config.load();
-        var row = $("div.list_item");
-        var item = $("div.col-md-12");
-        item.each(function(){
-          $("<span/>")
-            .addClass("item_size")
-            .addClass("pull-right")
-            .width("80")
-            .appendTo(this);
-        });
-
-        var path = $(".item_link").attr("href");
-
-        var filesize = new Array();
-        for(var i = 0; i < path.length; ++i){
-          filesize[i] = $.get(utils.url_path_join(base_url, "filesize", path[i]));
-        }
-
-        var item_size = $(".item_size");
-        for(var i = 0; i < item_size_array.length; ++i){
-          item_size[i].text(filesize[i]);
-        }
         /**
         var handler = function () {
             alert('this is an alert from my_extension!');
