db6d91a2cc0531c1ecddf23758b866414f8091f9
iteratorlee <1400012951@pku.edu.cn>
2017-04-21 20:18:19 -0700

    add auto loading when scroll down

--- front/static/table/js/table.js
+++ front/static/table/js/table.js
@@ -20,14 +20,15 @@ require([
 
     console.log(filename);
     
-    var req_url = get_base_url(curr_url) + "file_content/" + filename + "/0/1000";
+    var req_url = get_base_url(curr_url) + "file_content/" + filename + "/1/1000";
     $.get(req_url, function(data, status, datatype="text"){
         var container = document.getElementById("table_content");
         var temp_arr = data.split('\n');
         var table_data = new Array();
         
         for(var i = 0; i < temp_arr.length; ++i){
-            table_data[i] = temp_arr[i].split(',');
+            if(temp_arr[i] != "")
+                table_data[i] = temp_arr[i].split(',');
         }
 
         var table = new Handsontable(container, {
@@ -39,15 +40,39 @@ require([
             height: $(document).height()
         });
         
-        var editor = function(){
-            this.table = table;
-            this.loadlines = 1000;
-            this.tabledata = data;
-        }
+        var editor = new Object();
+        editor.table = table;
+        editor.loadlines = 1000;
+        editor.tabledata = table_data;
 
 
         Handsontable.hooks.add('afterScrollVertically', function(){
             //auto loading need to be added
+            var x = editor.table.rowOffset();
+            console.log(x);
+            if(x > editor.loadlines - 100){
+                editor.loadlines += 100;
+                var _curr_url = document.URL;
+                var _curr_url_arr = _curr_url.split('/');
+                var _filename = _curr_url_arr[_curr_url_arr.length-1];
+                
+                var dreq_url = get_base_url(document.URL) + "file_content/" + _filename + "/" + (editor.loadlines-100) + "/" + editor.loadlines;
+                
+                $.get(dreq_url, function(_data, _status, datatype="text"){
+                    var _temp_arr = _data.split('\n');
+                    var _table_data = new Array();
+
+                    for(var i = 0; i < _temp_arr.length; ++i){
+                        if(_temp_arr[i] != "")
+                            _table_data[i] = _temp_arr[i].split(',');
+                    }
+                    
+                    _table_data = editor.tabledata.concat(_table_data);
+                    editor.table.loadData(_table_data);
+                    editor.tabledata = _table_data;
+                    editor.table.save_enabled = true;
+                });
+            }
         }, table);
     });
 });
